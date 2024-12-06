cb2b00ed934521bd720b0faf784b9ff20d4f5339
M. Mousa Naeem <122889315+muhammad-mousa@users.noreply.github.com>
2024-12-06 22:15:15 +0500

    implement theme selection using shadcn (#2)

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,4 +1,6 @@
 "use client"
+import { Button } from "@/components/ui/button";
+import { DESTRUCTION } from "dns";
 import { useRouter } from "next/navigation"
 export default function About()
 {
@@ -7,6 +9,7 @@ export default function About()
         <div className="container">            
             <h1>About us</h1>
             <button onClick={() => router.push("/")} className="bg-blue-500 text-white p-2 rounded-md" >Go home</button>
+            <Button variant="destructive" onClick={() => router.push("/product/1")}  >Go to Product</Button>
         </div>
     );
 }
