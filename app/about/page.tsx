337ecfd9299a44dd6d43e45a69f144b05092e299
Muhammad Mousa <ranamousa030397@gmail.com>
2024-12-25 08:23:39 +0500

    remove eslint errors

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,12 +1,11 @@
 "use client"
 import { Button } from "@/components/ui/button";
-import { DESTRUCTION } from "dns";
 import { useRouter } from "next/navigation"
 export default function About()
 {
     const router = useRouter();
-    return ( 
-        <div className="container">            
+    return (
+        <div className="container">
             <h1>About us</h1>
             <button onClick={() => router.push("/")} className="bg-blue-500 text-white p-2 rounded-md" >Go home</button>
             <Button variant="destructive" onClick={() => router.push("/product/1")}  >Go to Product</Button>
