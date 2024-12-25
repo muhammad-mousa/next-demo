fb8db409a967cbfea4c7ef3854c2a77d9fa1496c
Muhammad Mousa <ranamousa030397@gmail.com>
2024-12-25 08:38:19 +0500

    remove lint errors

--- app/product/[id]/layout.tsx
+++ app/product/[id]/layout.tsx
@@ -3,12 +3,12 @@ import React from "react";
 export default function productlayout({
 children,
 }: {
-   children: React.ReactNode; 
+children: React.ReactNode;
 })  {
     return (
-        <div> 
-            {children}        
-            <div> 
+        <div>
+            {children}
+            <div>
                 <h2>Featured products section.</h2>
             </div>
         </div>
